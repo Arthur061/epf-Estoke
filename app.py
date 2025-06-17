03296c5faf4356116ff2144605fbf7efa06f2249
Arthur Alves <arthurgostoso78@gmail.com>
2025-06-17 16:44:07 -0300

    alterações minimas conforme necesário para inicio do programa

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from bottle import Bottle
+from beaker.middleware import SessionMiddleware
 from config import Config
 
 class App:
     def __init__(self):
         self.bottle = Bottle()
         self.config = Config()
+        self.bottle.TEMPLATE_PATH = [self.config.TEMPLATE_PATH]
 
+    # conf da sessão 1h
+        self.session_opts = {
+            'session.type': 'file',  
+            'session.data_dir': './data/sessions',
+            'session.cookie_expires': 3600,  
+            'session.auto': True,
+            'session.secret': self.config.SECRET_KEY
+        }
+
+        self.app = SessionMiddleware(self.bottle, self.session_opts)
 
     def setup_routes(self):
         from controllers import init_controllers
@@ -16,12 +28,12 @@ class App:
 
     def run(self):
         self.setup_routes()
-        self.bottle.run(
+        from bottle import run
+        run(app=self.app, 
             host=self.config.HOST,
             port=self.config.PORT,
             debug=self.config.DEBUG,
-            reloader=self.config.RELOADER
-        )
+            reloader=self.config.RELOADER)
 
 
 def create_app():
--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,5 +1,7 @@
 from bottle import Bottle
-from controllers.user_controller import user_routes
+from .auth_controller import auth_routes
+from .user_controller import user_routes
 
 def init_controllers(app: Bottle):
+    app.merge(auth_routes)
     app.merge(user_routes)
