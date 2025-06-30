388d596e6325e8959f6f03e67a06a534c9325bae
Arthur Alves <arthurgostoso78@gmail.com>
2025-06-30 14:29:52 -0300

    Arquivo ficou inultilizado

--- controllers/__init__.py
+++
@@ -1,7 +0,0 @@
-from bottle import Bottle
-from .auth_controller import auth_routes
-from .user_controller import user_routes
-
-def init_controllers(app: Bottle):
-    app.merge(auth_routes)
-    app.merge(user_routes)
